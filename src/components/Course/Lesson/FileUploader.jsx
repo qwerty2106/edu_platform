20e013fbe0bec92a5617073c6be61c3b97386eae
Anna <imshrtlvv@yandex.ru>
2025-11-16 13:55:19 +0300

    file uploader added

--- src/components/Course/Lesson/FileUploader.jsx
+++ src/components/Course/Lesson/FileUploader.jsx
@@ -1,10 +1,13 @@
 import { FilePond, registerPlugin } from "react-filepond";
-import 'filepond/dist/filepond.min.css';
 import { useState } from "react";
 import { Button } from "react-bootstrap";
 import { useDispatch } from "react-redux";
 import { setNotify } from "../../../redux/app-reducer";
+import FilePondPluginFileEncode from 'filepond-plugin-file-encode';
 
+import 'filepond/dist/filepond.min.css';
+
+registerPlugin(FilePondPluginFileEncode);
 
 const FileUploader = () => {
     const dispatch = useDispatch();
@@ -13,13 +16,14 @@ const FileUploader = () => {
     const onClickHandle = () => {
         const file = files[0];
         const fileName = file.file.name.toLowerCase();
+        const fileSize = file.file.size / 1024 / 1024;  //size всегда в байтах
 
-        const isValid = fileName.endsWith('.rar') || fileName.endsWith('.zip') || fileName.endsWith('.7z');
+        const isValid = (fileName.endsWith('.rar') || fileName.endsWith('.zip') || fileName.endsWith('.7z')) && fileSize <= 50; //50MB лимит
 
-        if (isValid) 
+        if (isValid)
             dispatch(setNotify({ status: 'success', message: 'Файл успешно отправлен!' }));
-        else 
-            dispatch(setNotify({ status: 'error', message: 'Неверный формат файла!' }));
+        else
+            dispatch(setNotify({ status: 'error', message: 'Ошибка отправки файла!' }));
     }
     return (
         <div>
@@ -28,12 +32,17 @@ const FileUploader = () => {
                 onupdatefiles={setFiles}
                 allowMultiple={false}
                 maxFiles={1}
-                maxFileSize="50MB"
+                maxFileSize="500KB"
                 credits={false}
                 labelIdle='Перетащите ZIP файл или <span class="filepond--label-action">выберите</span>'
-                labelFileTypeNotAllowed="Некорректный формат файла!"
-                fileValidateTypeLabelExpectedTypes="Ожидается ZIP файл"
-
+                allowFileEncode={true}
+                allowProcess={false}
+                instantUpload={false}
+                labelFileLoading="Загрузка..."
+                labelTapToCancel="Нажмите для отмены"
+                labelFileLoadError="Ошибка загрузки"
+                labelTapToRetry="Нажмите для повтора"
+                labelFileWaitingForSize="Определяем размер файла..."
             />
             <Button variant="success" disabled={files.length === 0} onClick={onClickHandle}>Отправить</Button>
         </div>
